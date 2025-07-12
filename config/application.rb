42737210f2143cd967e1bdb6a3540db56e920ae4
consmas <consmasgh@gmail.com>
2025-07-12 12:24:42 +0000

    Guard dotenv to dev/test only

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,11 @@ require "action_cable/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
+if Rails.env.development? || Rails.env.test?
+  require "dotenv/rails-now"   # or "dotenv/rails" if you prefer
+end
+
 Bundler.require(*Rails.groups)
 
 module Backend
